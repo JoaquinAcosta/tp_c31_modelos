18650823c6a51ab94389e6b669b4b4a5e66d0609
JoaquinAcosta <joaquinnof14@gmail.com>
2022-09-13 16:37:13 -0300

    add sequelize and models movie-genre

---
+++ src/controllers/moviesController.js
@@ -0,0 +1,13 @@
+const db = require('../database/models');
+
+module.exports = {
+    list : (req,res) => {
+        db.Movie.findAll()
+            .then((movies) => {
+                return res.send(movies)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }
+}
